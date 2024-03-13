d947ea86dc703b3be27879a0104e5e71422aa2b7
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-03-13 13:53:16 +0100

    algunos atributos añadidos

--- src/Modelo/Arma.java
+++ src/Modelo/Arma.java
@@ -2,4 +2,36 @@ package Modelo;
 
 public class Arma {
 
+	private String tipo;
+	private int daño;
+	
+	public Arma() {
+		
+	}
+	
+	public Arma(String tipo, int daño) {
+		super();
+		this.tipo = tipo;
+		this.daño = daño;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public int getDaño() {
+		return daño;
+	}
+	public void setDaño(int daño) {
+		this.daño = daño;
+	}
+
+	@Override
+	public String toString() {
+		return "Arma [tipo=" + tipo + ", daño=" + daño + "]";
+	}
+	
+	
 }
--- src/Modelo/Caballero.java
+++ src/Modelo/Caballero.java
@@ -2,4 +2,9 @@ package Modelo;
 
 public class Caballero {
 
+	private String nombre;
+	private Arma arma;
+	private Escudo escudo;
+	private int experiencia;
+	private int dañoCaballero;
 }
