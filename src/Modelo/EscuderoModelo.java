0637e7ae1ecc5d3e2915ded8401bcf8d85ff60ae
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-03-14 12:31:38 +0100

    insert de escudero a medias

--- src/Modelo/EscuderoModelo.java
+++ src/Modelo/EscuderoModelo.java
@@ -1,7 +1,26 @@
 package Modelo;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import Conector.Conector;
 
 public class EscuderoModelo extends Conector{
 
+	public static void insertarEscudero(Escudero escudero) {
+		String sql = "INSERT INTO clientes (Id_escudero, Nombre, Experiencia) VALUES (?, ?, ?)";
+		
+		try {
+			PreparedStatement pst = cn.prepareStatement(sql);
+			pst.setInt(1, escudero.getIdEscudero());
+			pst.setString(2, escudero.getNombre());
+			pst.setInt(3, escudero.getExperiencia());
+			
+			pst.execute();
+		} 	
+			catch (SQLException e) {
+			System.out.println("Error en la Query");
+			e.printStackTrace();
+		}
+	}
 }
