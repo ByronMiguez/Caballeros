d6f283989caf886db4d6e737d59c783f522f18ec
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-03-16 11:35:47 +0100

    arreglo de daño a danio, arma CRUID terminado

--- src/Modelo/ArmaModelo.java
+++ src/Modelo/ArmaModelo.java
@@ -1,28 +1,85 @@
 package Modelo;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class ArmaModelo extends Conector{
 
 	public static void insertArma(Arma arma) {
 		
-		String sql= "insert into arma(tipo, daño) values(?,?)";
+		String sql= "insert into arma(tipo, danio) values(?,?)";
+		
 		try {
-			System.out.println("antes de ejecutar");
 			PreparedStatement pst = cn.prepareStatement(sql);
-			
 			pst.setString(1, arma.getTipo());
-			pst.setInt(2, arma.getDaño());
-			
+			pst.setInt(2, arma.getDanio());
 			pst.execute();
+			
 		} catch (SQLException e) {
-			System.out.println("error sql");
+			System.out.println("error sql insertar");
 			e.printStackTrace();
 		}
 	}
-	public static void deleteArma(Arma arma) {
+	public static void deleteArma(int idArma) {
 		
+		String sql ="delete from arma where id=?";
+		 
+		try {
+			PreparedStatement pst = cn.prepareStatement(sql);
+			pst.setInt(1, idArma);
+			pst.execute();
+			
+		} catch (SQLException e) {
+			System.out.println("error sql eliminar");
+			e.printStackTrace();
+		}
 	}
+	//Recibe un id y un arma, se modifica la fila que corresponda con el id dado, con la información del arma recibido.
+	public static void updateArma(int id, Arma arma) {
+		
+		String sql ="update arma set tipo=?,danio=? where id=?";
+		
+		try {
+			PreparedStatement pst = cn.prepareStatement(sql);
+			pst.setString(1, arma.getTipo());
+			pst.setInt(2, arma.getDanio());
+			pst.setInt(3, id);
+			pst.execute();
+			
+		} catch (SQLException e) {
+			System.out.println("error sql modificar");
+			e.printStackTrace();
+		}	
+	}
+	public  ArrayList<Arma> mostrarArmas() {
+		
+		ArrayList<Arma> listaArmas = new ArrayList<>();
+		String sql = "select * from arma";
+		
+		try {
+			Statement st = cn.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+			while (rs.next()) {
+				Arma arma = new Arma();
+				
+				arma.setIdArma(rs.getInt(1));
+				arma.setTipo(rs.getString(2));
+				arma.setDanio(rs.getInt(3));
+				
+				listaArmas.add(arma);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("error sql mostrar");			
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
 
 }
