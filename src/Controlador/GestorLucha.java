17cddfdde2029278a56f9d0d67c75fd5ca6ca43a
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-03-18 13:09:53 +0100

    GestorLucha sin ninguna funcion pero empezado

--- src/Controlador/GestorLucha.java
+++ src/Controlador/GestorLucha.java
@@ -1,5 +1,13 @@
 package Controlador;
 
+import java.util.Scanner;
+
+import Modelo.CaballeroModelo;
+import Modelo.LuchaModelo;
+import Vista.Formulario;
+import Vista.Menu;
+import Vista.Visor;
+
 public class GestorLucha {
 
 	public static void main(String[] args) {
@@ -8,5 +16,36 @@ public class GestorLucha {
 
 	public static void run() {
 	
+		Scanner scan = new Scanner(System.in);
+		int opcion;
+		
+		do {
+			Menu.menuLucha();
+			
+			System.out.println("Ingrese su opcion");
+			
+			opcion= Integer.parseInt(scan.nextLine());
+			
+			switch (opcion) {
+			
+				case Menu.LUCHA_ELIGIENDO:
+						LuchaModelo.conectar();
+						LuchaModelo.cerrar();
+					break;
+				case Menu.LUCHA_RANDOM:
+						LuchaModelo.conectar();
+						LuchaModelo.cerrar();
+						break;
+				default:
+					break;
+			}
+		
+		} while(opcion!=0);
+		  System.out.println("ADIOS");
+	      scan.close();
 	}
 }
+	
+	
+
+
